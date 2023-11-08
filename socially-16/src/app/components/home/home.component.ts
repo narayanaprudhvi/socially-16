f172e49c419d8778e03a34026d0e902647c32235
cherryamu12@gmail.com <58223169+narayanaprudhvi@users.noreply.github.com>
2023-11-08 09:50:32 +0530

    uii

--- socially-16/src/app/components/home/home.component.ts
+++ socially-16/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,91 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  currentDateTime: Date = new Date();
 
+  feedsForm : FormGroup;
+  feedData : any ={};
+
+  postsArray : any =[
+    {
+      profileName : 'Narayana Prudhvi',
+      profileNickName : 'nP',
+      profileTime : '25',
+      profilePostContent : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam facilis maiores quas corporis, quidem officiis harum commodi similique dolores excepturi nihil earum dolore fuga. Temporibus aspernatur culpa accusantium perferendis consequatur',
+    },
+    {
+      profileName : 'Minnu',
+      profileNickName : 'nM',
+      profileTime : '25',
+      profilePostContent : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam facilis maiores quas corporis, quidem officiis harum commodi similique dolores excepturi nihil earum dolore fuga. Temporibus aspernatur culpa accusantium perferendis consequatur',
+    },
+    {
+      profileName : 'Venkat Raju',
+      profileNickName : 'V R',
+      profileTime : '40',
+      profilePostContent : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam facilis maiores quas corporis, quidem officiis harum commodi similique dolores excepturi nihil earum dolore fuga. Temporibus aspernatur culpa accusantium perferendis consequatur'
+    },
+    {
+      profileName : 'Ashok Kumar',
+      profileNickName : 'A K',
+      profileTime : '50',
+      profilePostContent : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam facilis maiores quas corporis, quidem officiis harum commodi similique dolores excepturi nihil earum dolore fuga. Temporibus aspernatur culpa accusantium perferendis consequatur'
+    },
+    {
+      profileName : 'Uday Kumar',
+      profileNickName : 'U K',
+      profileTime : '12',
+      profilePostContent : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam facilis maiores quas corporis, quidem officiis harum commodi similique dolores excepturi nihil earum dolore fuga. Temporibus aspernatur culpa accusantium perferendis consequatur'
+    },
+    {
+      profileName : 'Shivani',
+      profileNickName : 'S V',
+      profileTime : '45',
+      profilePostContent : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam facilis maiores quas corporis, quidem officiis harum commodi similique dolores excepturi nihil earum dolore fuga. Temporibus aspernatur culpa accusantium perferendis consequatur'
+    },
+    {
+      profileName : 'Gayathri',
+      profileNickName : 'G',
+      profileTime : '22',
+      profilePostContent : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam facilis maiores quas corporis, quidem officiis harum commodi similique dolores excepturi nihil earum dolore fuga. Temporibus aspernatur culpa accusantium perferendis consequatur'
+    },
+    {
+      profileName : 'Adithya',
+      profileNickName : 'A B D',
+      profileTime : '32',
+      profilePostContent : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam facilis maiores quas corporis, quidem officiis harum commodi similique dolores excepturi nihil earum dolore fuga. Temporibus aspernatur culpa accusantium perferendis consequatur'
+    },
+  ]
+
+  constructor(private fb : FormBuilder) {
+    this.feedsForm = this.fb.group({
+    })
+   }
+
+  ngOnInit() {
+    this.updateDateTime();
+    setInterval(() => {
+      this.updateDateTime();
+    }, 1000);
+  }
+
+  updateDateTime() {
+    this.currentDateTime = new Date();
+  }
+
+  like : boolean = false;
+  toggleLikee(x:any){
+    x.like = ! x.like;
+  }
+  disLike : boolean = true;
+
+  disLikee(x:any){
+    x.disLike = ! x.disLike;
+   }
+
+   displayFullText : boolean = false;
+
+   toggleDisplay(i:any){
+    this.displayFullText = ! this.displayFullText;
+   }
 }
